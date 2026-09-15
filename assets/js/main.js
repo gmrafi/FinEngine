24378a7da91b64571acb1f185ff6982d1d55d039
Md Golam Mubasshir Rafi <gmrafi45@gmail.com>
2026-09-15 11:31:05 +0000

    Hero r13: real two-column desktop hero (copy left / quickstart right, <=1024px stacked) + remove Contact from header nav + cache-bust 20260915i

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 import { SITE_BRAND, applyBrand } from './brand.js';
-import { initSharedLayout } from './shared-layout.js?v=20260915h';
+import { initSharedLayout } from './shared-layout.js?v=20260915i';
 import { initThemeToggle } from './modules/theme.js';
 import { initScrollSpy } from './modules/nav.js';
 import { initReveal } from './modules/reveal.js';
