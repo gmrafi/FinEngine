53ec6270e1e3edf76db288190fd6ce44092dbc2d
Md Golam Mubasshir Rafi <gmrafi45@gmail.com>
2026-09-15 11:03:20 +0000

    Hero flagship r8: status badge + specific value-prop headline + flatten cards + dark terminal with shell/JS/CDN tabs (split copy panes)

--- assets/js/doc-page.js
+++ assets/js/doc-page.js
@@ -1,5 +1,5 @@
 import { applyBrand } from './brand.js';
-import { initSharedLayout } from './shared-layout.js?v=20260915c';
+import { initSharedLayout } from './shared-layout.js?v=20260915d';
 import { initThemeToggle } from './modules/theme.js';
 import { initCodeCopy } from './modules/code-copy.js';
 
