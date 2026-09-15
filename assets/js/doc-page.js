32149a8319bc01e23db2836c153ca58b2b34c16d
Md Golam Mubasshir Rafi <gmrafi45@gmail.com>
2026-09-15 10:55:06 +0000

    Bump cache-bust to v20260915c for Labs sprint rollout

--- assets/js/doc-page.js
+++ assets/js/doc-page.js
@@ -1,5 +1,5 @@
 import { applyBrand } from './brand.js';
-import { initSharedLayout } from './shared-layout.js?v=20260915b';
+import { initSharedLayout } from './shared-layout.js?v=20260915c';
 import { initThemeToggle } from './modules/theme.js';
 import { initCodeCopy } from './modules/code-copy.js';
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 import { SITE_BRAND, applyBrand } from './brand.js';
-import { initSharedLayout } from './shared-layout.js?v=20260915b';
+import { initSharedLayout } from './shared-layout.js?v=20260915c';
 import { initThemeToggle } from './modules/theme.js';
 import { initScrollSpy } from './modules/nav.js';
 import { initReveal } from './modules/reveal.js';
